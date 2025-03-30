59978e85d26305b0f73dccf8f69fa547939efc67
Dhaval8 <dhavalmehta5604@gmail.com>
2025-03-30 21:23:33 +0530

    Commit

--- VehicleKilometerTracking/src/main/java/com/example/VehicleKilometerTracking/repository/VehicleKilometerTrackingRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.VehicleKilometerTracking.repository;
-
-import com.example.VehicleKilometerTracking.entity.VehicleKilometerTracking;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VehicleKilometerTrackingRepository extends JpaRepository<VehicleKilometerTracking, Integer> {}
