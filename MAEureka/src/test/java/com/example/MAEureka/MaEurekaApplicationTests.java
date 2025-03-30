5dfd47dac2e43f734e76b5b20cb7abed58abdb44
anand <anand.panchdhari@gmail.com>
2025-03-30 23:06:41 +0530

    Added swagger dependency and straightened out the ports

---
+++ MAEureka/src/test/java/com/example/MAEureka/MaEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MAEureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
