dc5674c5444703eb59c3afd685c8477cb8b86fe4
anand <anand.panchdhari@gmail.com>
2025-04-02 23:59:34 +0530

    all but api gateway working

--- VehicleKilometerTracking/src/main/java/com/example/VehicleKilometerTracking/client/FeignClientVehicle.java
+++ VehicleKilometerTracking/src/main/java/com/example/VehicleKilometerTracking/client/FeignClientVehicle.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "vehicle", url = "localhost:8083")
+@FeignClient(name = "vehicle", url = "http://localhost:8085")
 public interface FeignClientVehicle {
     @GetMapping("/{id}")
     VehicleDTO getVehicleById(@PathVariable int id);
