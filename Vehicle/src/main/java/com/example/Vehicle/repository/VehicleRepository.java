aaaa48762384b21d65624bd1e976add28b840e68
Anand Panchdhari <anand.panchdhari@gmail.com>
2025-04-02 16:52:32 +0530

    Added a temporary fix to the service. Have to check if it still works.

--- Vehicle/src/main/java/com/example/Vehicle/repository/VehicleRepository.java
+++ Vehicle/src/main/java/com/example/Vehicle/repository/VehicleRepository.java
@@ -2,5 +2,7 @@ package com.example.Vehicle.repository;
 
 import com.example.Vehicle.entity.VehicleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface VehicleRepository extends JpaRepository<VehicleEntity, Integer> {}
